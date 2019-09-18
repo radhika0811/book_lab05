3f29f57834d3bf7af8ff55ce43de52975b685ca8
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2019-09-17 20:34:59 -0700

    constructor made

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,7 +1,25 @@
 package test;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        if (n == 0) {
+            System.out.println("N/A");
+        } else {
+            s.nextLine().trim();
+            for (int i = 1; i <= n; i++) {
+                String bookname = s.nextLine();
+                String authorname = s.nextLine();
+                String isbn = s.nextLine();
+                Book obj = new Book(String bookname, String authorname, String isbn);
+                obj.Bookdetails();
+
+            }
+        }
 
     }
 }
@@ -33,6 +51,14 @@ class Book {
 
     public void setIsbn(String i) {
         authorname = i;
+    }
+
+    void Bookdetails() {
+        System.out.print("-----------------------------" + "\n"
+                "Bookname\t" + bookname + "\n"
+                "Authorname\t" + authorname + "\n"
+                "Isbn\t" + isbn + "\n"
+                "-----------------------------");
     }
 
 
